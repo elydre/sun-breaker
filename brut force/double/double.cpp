9b8ba6860f2464cc0f8f924aaedb929519e3e523
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-08-17 17:19:46 +0200

    pf4

---
+++ brut force/double/double.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+
+using namespace std; 
+
+int main()
+{
+    int jam = 0;
+    string myints[] = {};
+    std::list<string> todo (myints, myints + sizeof(myints) / sizeof(string));
+    ifstream fichier("s.txt");
+    if(fichier)
+    {
+        //L'ouverture s'est bien passée, on peut donc lire
+
+        string ligne; //Une variable pour stocker les lignes lues
+
+        while(getline(fichier, ligne)) //Tant qu'on n'est pas à la fin, on lit
+        {
+            todo.emplace_back(ligne);
+        }
+    }
+    else
+    {
+        cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
+    }
+
+    for (std::list<string>::iterator a = todo.begin(); a != todo.end(); a++)
+    {
+        jam = 0;
+        for (std::list<string>::iterator b = todo.begin(); b != todo.end(); b++)
+        {
+            if (*a == *b)
+            {
+                ++jam;
+            }
+        }
+        if (jam > 1)
+        {
+            std::cout << jam << " - " << *a << std::endl;
+        }
+    }
+
+    return 0;
+}
