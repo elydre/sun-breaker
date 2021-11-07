46b39db68fb29c3390b109c753cba1744b46a450
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-11-07 17:26:08 +0100

    cli push

--- step/step.py
+++ step/step.py
@@ -1,1 +1,53 @@
-import mod.sunbreaker as sunbreaker
+from mod.sunbreaker import *
+from mod.ColorPrint import colorprint, Colors
+
+var = input("Texte: ")
+
+var = TtoB(var)
+
+colorprint("var = TtoB(var) -> ", color = Colors.blanc,end = False)
+colorprint(var, color = Colors.cyan)
+
+var = BtoC(var)
+
+colorprint("var = BtoC(var) -> ", color = Colors.blanc,end = False)
+colorprint(var, color = Colors.cyan)
+
+var = TtoB(var)
+
+old = var
+
+colorprint("var = TtoB(var) -> ", color = Colors.blanc,end = False)
+colorprint(var, color = Colors.cyan)
+
+clé = BtoC(var)
+
+colorprint("clé = BtoC(var) -> ", color = Colors.blanc,end = False)
+colorprint(clé, color = Colors.cyan)
+
+sort = ""
+passed = 0
+while len(clé) < len(var): clé += clé
+
+colorprint("var = pert(var) -> ", color = Colors.blanc,end = False)
+
+for x in range(len(var)):
+    passed += 1
+
+    if int(passed) > int(clé[x]):
+        colorprint(var[x], color = Colors.red, end = False)
+        passed = 0
+
+    else:
+        sort += str(var[x])
+        colorprint(var[x], color = Colors.cyan, end = False)
+
+var = sort
+
+colorprint("\n                -> ", color = Colors.blanc,end = False)
+colorprint(var, color = Colors.cyan)
+
+var = BtoN(var)
+
+colorprint("var = BtoN(var) -> ", color = Colors.blanc,end = False)
+colorprint(str(var), color = Colors.cyan)
