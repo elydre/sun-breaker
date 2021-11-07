78b66ae4a2b52132100aa853193c06a8d03a7786
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-11-07 18:18:30 +0100

    cli push

--- step/up.py
+++ step/up.py
@@ -1,44 +1,28 @@
 from mod.sunbreaker import *
 from mod.ColorPrint import colorprint, Colors
+import random
 
-var = input("Texte: ")
+var = "".join(random.choice("1234") for _ in range(random.randint(1, 4)))
+
+print("Texte:",var)
 
 var = TtoB(var)
 
 old = var
 
-colorprint("var = TtoB(var) -> ", color = Colors.blanc,end = False)
-colorprint(var, color = Colors.cyan)
-
 clé = BtoC(var)
 
-colorprint("clé = BtoC(var) -> ", color = Colors.blanc,end = False)
-colorprint(clé, color = Colors.cyan)
 
 sort = ""
 passed = 0
 while len(clé) < len(var): clé += clé
 
-colorprint("var = pert(var) -> ", color = Colors.blanc,end = False)
-
 for x in range(len(var)):
     passed += 1
-
-    # colorprint(f"[{passed}~{clé[x]}]", color = Colors.green, end = False)
-
-    if int(passed) > int(clé[x]):
-        colorprint(var[x], color = Colors.red, end = False)
-        passed = 0
-
-    else:
-        sort += str(var[x])
-        colorprint(var[x], color = Colors.cyan, end = False)
+    if int(passed) > int(clé[x]): passed = 0
+    else:  sort += str(var[x])
 
 var = sort
-
-colorprint("\n                -> ", color = Colors.blanc,end = False)
-colorprint(var, color = Colors.cyan)
-
 var = BtoN(var)
 
 colorprint("var = BtoN(var) -> ", color = Colors.blanc,end = False)
