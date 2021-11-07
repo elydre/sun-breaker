801fdfd8dc510618dec1ed2be9bd4437d82fd2ed
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-11-07 18:21:00 +0100

    cli push

--- step/up.py
+++ step/up.py
@@ -2,28 +2,28 @@ from mod.sunbreaker import *
 from mod.ColorPrint import colorprint, Colors
 import random
 
-var = "".join(random.choice("1234") for _ in range(random.randint(1, 4)))
+while True:
+    var = "".join(random.choice("1234") for _ in range(random.randint(1, 4)))
 
-print("Texte:",var)
+    print("Texte:",var)
 
-var = TtoB(var)
+    var = TtoB(var)
 
-old = var
+    old = var
 
-clé = BtoC(var)
+    clé = BtoC(var)
 
 
-sort = ""
-passed = 0
-while len(clé) < len(var): clé += clé
+    sort = ""
+    passed = 0
+    while len(clé) < len(var): clé += clé
 
-for x in range(len(var)):
-    passed += 1
-    if int(passed) > int(clé[x]): passed = 0
-    else:  sort += str(var[x])
+    for x in range(len(var)):
+        passed += 1
+        if int(passed) > int(clé[x]): passed = 0
+        else:  sort += str(var[x])
 
-var = sort
-var = BtoN(var)
+    var = sort
+    var = BtoN(var)
 
-colorprint("var = BtoN(var) -> ", color = Colors.blanc,end = False)
-colorprint(str(var), color = Colors.cyan)
+    colorprint(str(var), color = Colors.cyan)
