8079a993e19cdb1934dc92b49945dae0c5b5c104
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-17 16:36:56 +0100

    Update moonbreaker.py

--- moon-breaker/mod/moonbreaker.py
+++ moon-breaker/mod/moonbreaker.py
@@ -15,17 +15,33 @@
 
 version = "0.2"
 
+def BtoI(bina):
+    return int(bina,2)
+
 def TtoB(text):
     return(''.join(format(ord(i), '08b') for i in text))
 
 def MakeKey(bina,key):
     basse = 1
+    sup = BtoI(bina)
     for x in range(2,len(bina)*key + 20):
         basse *= int(bina[x%(len(bina))])+1
+
+    while True:
+        if sup > basse:
+            sup /= 2
+        elif sup*10 < basse:
+            sup *= 2
+        else:
+            break
+    
+    basse -= sup
+    
     while True:
         if len(str(basse)) > 10: basse //= 10
         elif len(str(basse)) < 10: basse *= 10
         else: break
+    
     return basse
         
 
