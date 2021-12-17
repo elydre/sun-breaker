d1be5fbe3f50dc173634b154af011f0f9dc593ee
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-17 16:31:48 +0100

    ah

--- sun-breaker/brut-force/double/double.py
+++ sun-breaker/brut-force/double/double.py
@@ -16,15 +16,11 @@ print(round(time()-debut,1),"s - fin,",len(lignes),"lignes!")
 print(round(time()-debut,1),"s - optimisation...")
 fichier = "" #on gagne de la ram
 print(round(time()-debut,1),"s - apprentisage...")
-todo = []
-for l in range(0,len(lignes)-1,2):
-    todo.append(int(lignes[l]))
+todo = [int(lignes[l]) for l in range(0,len(lignes)-1,2)]
 print(round(time()-debut,1),"s - fin,",len(todo),"break a tester!")
 
 for l in todo:
-    jam = 0
-    for m in todo:
-        if l == m: jam += 1      
+    jam = sum(l == m for m in todo)
     if jam > 1:
         cy_mkfil("/data","exploa.txt",cy_rfil_rela("/data","exploa.txt")+str(l)+";")
         cy_mkfil("/data","double.txt",cy_rfil_rela("/data","double.txt")+str(jam)+" - "+str(l)+"\n")
