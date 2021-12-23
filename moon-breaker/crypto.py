03c58ed35752174a0dfc77219d3dd13bad6b74a4
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-23 17:04:37 +0100

    Update crypto.py

--- moon-breaker/crypto.py
+++ moon-breaker/crypto.py
@@ -1,5 +1,12 @@
 from moonbreaker import moonbreaker
 
+# Break de 8 chiffres -> 0.0001 MoonCoin
+# Break de 7 chiffres -> 0.001  MoonCoin
+# Break de 6 chiffres -> 0.01   MoonCoin
+# Break de 5 chiffres -> 0.1    MoonCoin
+# Break de 4 chiffres -> 1      MoonCoin
+# Break de 3 chiffres -> 10     MoonCoin
+
 def nombre_en_lettres(nombre):
     sortie = ""
     lettres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
