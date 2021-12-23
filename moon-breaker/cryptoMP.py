c9184daa752c075ab1737617ec5a622134db1a48
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-23 17:52:06 +0100

    Update cryptoMP.py

--- moon-breaker/cryptoMP.py
+++ moon-breaker/cryptoMP.py
@@ -2,7 +2,19 @@ from moonbreaker import moonbreaker
 from multiprocessing import Pool
 from os import cpu_count
 
-def calcul(string):
+def nombre_en_lettres(nombre):
+    sortie = ""
+    lettres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+    while nombre > 0:
+        reste = nombre % len(lettres)
+        sortie += lettres[reste]
+        nombre = (nombre - reste) // len(lettres)
+
+    return sortie[::-1]
+
+def calcul(x):
+    string = nombre_en_lettres(x)
     return (string, len(str(moonbreaker(string))))
 
 
@@ -11,17 +23,6 @@ if __name__ == "__main__":
     cps = 100000 * cpu_count()
     wallet = 0
 
-    def nombre_en_lettres(nombre):
-        sortie = ""
-        lettres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-
-        while nombre > 0:
-            reste = nombre % len(lettres)
-            sortie += lettres[reste]
-            nombre = (nombre - reste) // len(lettres)
-
-        return sortie[::-1]
-
     i = 0
     while True:
         i += 1
@@ -29,7 +30,7 @@ if __name__ == "__main__":
         print(f"calcul de la serrie n°{i}, de '{nombre_en_lettres(cps*(i-1))}' à '{nombre_en_lettres(i * cps)}'")
 
         with Pool(cpu_count()) as pool:
-            result = pool.map(calcul, [nombre_en_lettres(x) for x in range(cps*(i-1), i * cps)])
+            result = pool.map(calcul, list(range(cps*(i-1), i * cps)))
 
         for string, longeur in result:
             if longeur < 10:
