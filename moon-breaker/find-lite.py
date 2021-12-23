a714969eadd919e578413ac278516e82c14739be
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-23 13:54:01 +0100

    Update find-lite.py

--- moon-breaker/find-lite.py
+++ moon-breaker/find-lite.py
@@ -11,12 +11,17 @@ def nombre_en_lettres(nombre):
 
     return sortie[::-1]
 
+def afficher_resultat(coups, mini):
+    print(f"{coups} - {nombre_en_lettres(coups)} | {mini[1]} -> {mini[0]}")
+
 acc = 0
-mini = 10**11
+mini = [10**11, ""]
 while True:
     string = nombre_en_lettres(acc)
     str_break = moonbreaker(string)
-    if str_break < mini:
-        mini = str_break
-        print(f"{string} -> {str_break}")
+    if str_break < mini[0]:
+        mini = [str_break, string]
+        afficher_resultat(acc, mini)
+    elif acc % 100000 == 0:
+        afficher_resultat(acc, mini)
     acc += 1
