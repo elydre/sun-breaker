400d7561113387100642f9f66d9695f5a0c10817
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-23 11:23:59 +0100

    Update find.py

--- moon-breaker/find.py
+++ moon-breaker/find.py
@@ -1,17 +1,40 @@
 from moonbreaker import moonbreaker
-from random import choice, randint
+from multiprocessing import Pool
 
-lettres = "abcdefghijklmnopqrstuvwxyz"
-best = 10**11
+def calcul(txt):
+    return (txt, moonbreaker(txt))
 
-def random_string():
-    length = randint(4, 6)
-    return "".join(choice(lettres) for i in range(length))
+if __name__ == "__main__":
+    maxi = 10000000
+    # transformé un nombre en suite de lettres
+    def nombre_en_lettres(nombre):
+        # 1 -> a
+        # 2 -> b
+        # 26 -> z
+        # 27 -> aa
 
+        sortie = ""
+        lettres = "abcdefghijklmnopqrstuvwxyz"
 
-while True:
-    string = random_string()
-    break_code = moonbreaker(string)
-    if break_code < best:
-        best = break_code
-        print(best, "|" ,string)
+        while nombre > 0:
+            reste = nombre % 26
+            sortie += lettres[reste]
+            nombre = (nombre - reste) // 26
+
+        return sortie[::-1]
+
+    print(f"generation des {maxi} strings")
+    totest = [nombre_en_lettres(s) for s in range(maxi)]
+    print(f"{totest[-1]} est le dernier")
+    
+    print("test en pool")
+
+    with Pool(processes=48) as pool:
+        result = pool.map(calcul, totest)
+
+    mini = 10**11
+
+    for txt, key in result:
+        if key < mini:
+            mini = key
+            print(f"{txt} -> {key}")
