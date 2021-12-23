1b3a43160864f1d1ded394ee73e4f61df23c47b2
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-23 16:02:08 +0100

    Create crypto.py

---
+++ moon-breaker/crypto.py
@@ -0,0 +1,23 @@
+from moonbreaker import moonbreaker
+
+def nombre_en_lettres(nombre):
+    sortie = ""
+    lettres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+    while nombre > 0:
+        reste = nombre % len(lettres)
+        sortie += lettres[reste]
+        nombre = (nombre - reste) // len(lettres)
+
+    return sortie[::-1]
+
+acc = 0
+wallet = 0
+while True:
+    string = nombre_en_lettres(acc)
+    str_break = moonbreaker(string)
+    if len(str(str_break)) < 10:
+        wallet = round(wallet + 2 ** (10 - len(str(str_break))) / 10000, 4)
+        print(f"Wallet : {wallet} MoonCoins  |   {string} => {str_break} ({len(str(str_break))})")
+
+    acc += 1
