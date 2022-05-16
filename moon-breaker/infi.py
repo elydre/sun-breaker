1a917455d03263e3871ca5bc8028a1e8eba29caf
pf4 <74263128+elydre@users.noreply.github.com>
2022-05-16 12:18:06 +0200

    Update infi.py

--- moon-breaker/infi.py
+++ moon-breaker/infi.py
@@ -2,23 +2,23 @@ from moonbreaker import moonbreaker
 from multiprocessing import Pool
 from random import choice
 
-def check_loop(string):
-    h = [string]
+def check_loop(info):
+    h = [info[0]]
     for i in range(10 ** 10):
-        temp = str(moonbreaker(h[-1], 42))
+        temp = str(moonbreaker(h[-1], info[1]))
         # print(temp)
         if temp not in h:
             h.append(temp)
         else:
-            return [i, string, h.index(temp)]
+            return [i, info[0], h.index(temp)]
 
 if __name__ == "__main__":
-    maxi, bs, sri = 0, "", 0
-    while True:
-        s = ["".join(choice("abcdefghijklmnopqrstuvwxyz") for _ in range(10)) for _ in range(10000)]
-        with Pool(processes=47) as pool:
+    maxi, bs = 0, ""
+    for sri in range(2, 500):
+        s = ["".join(choice("abcdefghijklmnopqrstuvwxyz") for _ in range(10)) for _ in range(3000)]
+        s = [[e, sri] for e in s]
+        with Pool(processes=4) as pool:
             res = pool.map(check_loop, s)
-        sri += 1
         print(f"---- {sri} ----")
         for i in res:
             if i[0] >= maxi:
