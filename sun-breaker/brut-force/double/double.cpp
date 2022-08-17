3017882b569541b96180615b3e98b7d7c983c61e
pf4 <74263128+elydre@users.noreply.github.com>
2022-08-17 16:09:42 +0200

    Update double.cpp

--- sun-breaker/brut-force/double/double.cpp
+++ sun-breaker/brut-force/double/double.cpp
@@ -5,42 +5,22 @@
 
 using namespace std; 
 
-int main()
-{
+int main() {
     int jam = 0;
     string myints[1] = {};
     list<string> todo (myints, myints + sizeof(myints) / sizeof(string));
     ifstream fichier("s.txt");
-    if(fichier)
-    {
-        //L'ouverture s'est bien passée, on peut donc lire
+    string ligne;
+    
+    if (fichier) while(getline(fichier, ligne)) todo.emplace_back(ligne);
+    else cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
 
-        string ligne; //Une variable pour stocker les lignes lues
-
-        while(getline(fichier, ligne)) //Tant qu'on n'est pas à la fin, on lit
-        {
-            todo.emplace_back(ligne);
-        }
-    }
-    else
-    {
-        cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
-    }
-
-    for (list<string>::iterator a = todo.begin(); a != todo.end(); a++)
-    {
+    for (list<string>::iterator a = todo.begin(); a != todo.end(); a++) {
         jam = 0;
-        for (list<string>::iterator b = todo.begin(); b != todo.end(); b++)
-        {
-            if (*a == *b)
-            {
-                ++jam;
-            }
+        for (list<string>::iterator b = todo.begin(); b != todo.end(); b++) {
+            if (*a == *b) ++jam;
         }
-        if (jam > 1)
-        {
-            cout << jam << " - " << *a << endl;
-        }
+        if (jam > 1) cout << jam << " - " << *a << endl;
     }
 
     return 0;
